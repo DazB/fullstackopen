236b5535bfd7887ba64e2fbac5fffb5b1211e4dc
Dariush Bahri <dazb@alumni.york.ac.uk>
2020-07-16 22:08:55 +0800

    1.6 unicafe step1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-const App = () => {
-  return (
-    <>
-      <h1>Hello bum</h1>
-      <Hello name="Maya" age={26 + 10} />
-    </>
+const Button = (props) => {
+  console.log('value is: ', props)
+  const { onClick, text } = props
+  return(
+    <button onClick={onClick}>
+      {text}
+    </button>
   )
 }
 
+const App = (props) => {
+  const [value, setValue] = useState(10)
+  
+  const setToValue = (newValue) => () => {
+    setValue(newValue)
+  }
+  
+  return (
+    <div>
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
+    </div>
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
