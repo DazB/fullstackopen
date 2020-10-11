09f61730a26e12aa125764768e954ef19975b95d
Dariush Bahri <dazb@alumni.york.ac.uk>
2020-10-11 22:21:47 +0800

    part5-11

---
+++ part2/test/cypress/integration/note_app.spec.js
@@ -0,0 +1,90 @@
+describe('Note app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'bum',
+      username: 'bum',
+      password: 'bum',
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function () {
+    cy.contains('Notes')
+    cy.contains(
+      'Note app, Department of Computer Science, University of Helsinki 2020'
+    )
+  })
+
+  it('login form can be opened', function () {
+    cy.contains('login').click()
+  })
+
+  it('user can login', function () {
+    cy.contains('login').click()
+    cy.get('#username').type('bum')
+    cy.get('#password').type('bum')
+    cy.get('#login-button').click()
+
+    cy.contains('bum logged in')
+  })
+
+  it('login fails with wrong password', function () {
+    cy.contains('login').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+      .should('contain', 'wrong credentials')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+
+    cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+  })
+
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'bum', password: 'bum' })
+    })
+
+    it('a new note can be created', function () {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+
+    describe('and a note exists', function () {
+      beforeEach(function () {
+        cy.createNote({
+          content: 'another note cypress',
+          important: false,
+        })
+      })
+
+      it('it can be made important', function () {
+        cy.contains('another note cypress')
+          .parent()
+          .find('button')
+          .as('theButton')
+        cy.get('@theButton').click()
+        cy.get('@theButton').should('contain', 'make not important')
+      })
+    })
+    describe('and several notes exist', function () {
+      beforeEach(function () {
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
+      })
+
+      it('one of those can be made important', function () {
+        cy.contains('second note').parent().find('button').as('theButton')
+        cy.get('@theButton').click()
+        cy.get('@theButton').should('contain', 'make not important')
+      })
+    })
+  })
+})
