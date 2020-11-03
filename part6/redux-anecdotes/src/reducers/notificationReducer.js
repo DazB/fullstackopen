0d81c2b7df4e914c047bd5c1c21a75d28401a4b1
Dariush Bahri <dazb@alumni.york.ac.uk>
2020-11-03 21:43:13 +0800

    6.11 Better anecdotes, step9

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,5 +1,25 @@
-const notificationReducer = (state = 'BUM', action) => {
-  return state
+const notificationReducer = (state = null, action) => {
+  switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return action.notification
+    case 'CLEAR_NOTIFICATION':
+      return null
+    default:
+      return state
+  }
+}
+
+export const setNotification = (notification) => {
+  return {
+    type: 'SET_NOTIFICATION',
+    notification,
+  }
+}
+
+export const clearNotification = () => {
+  return {
+    type: 'CLEAR_NOTIFICATION',
+  }
 }
 
 export default notificationReducer
