f61dfecb82431eeab2fcc2c35d3dee8189b32abd
Dariush Bahri <dazb@alumni.york.ac.uk>
2020-11-05 22:25:25 +0800

    6.14 Anecdotes and the backend, step2

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -5,19 +5,28 @@ import {
   setNotification,
   clearNotification,
 } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = (props) => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-    dispatch(setNotification(`you added '${content}'`))
-    setTimeout(() => {
-      dispatch(clearNotification())
-    }, 5000)
+  const addAnecdote = async (event) => {
+    try {
+      event.preventDefault()
+      const content = event.target.anecdote.value
+      event.target.anecdote.value = ''
+      await anecdoteService.create(content)
+      dispatch(createAnecdote(content))
+      dispatch(setNotification(`you added '${content}'`))
+      setTimeout(() => {
+        dispatch(clearNotification())
+      }, 5000)
+    } catch (exception) {
+      dispatch(setNotification(`error adding anecdote`))
+      setTimeout(() => {
+        dispatch(clearNotification())
+      }, 5000)
+    }
   }
 
   return (
